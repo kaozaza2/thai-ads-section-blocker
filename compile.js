e195f395428fc2aa5109a93e42b072bce2669df3
Kittisak Phiri <kittisakphiri@gmail.com>
2022-12-16 00:09:18 +0700

    fix deploy

--- compile.js
+++ compile.js
@@ -8,11 +8,11 @@ subscriptions += header;
 
 const files = fs.readdirSync('filters');
 
-for (const file in files) {
-    const data = fs.readFileSync('header.txt', {encoding:'utf8', flag:'r'});
+files.forEach(file => {
+    const data = fs.readFileSync('filters/' + file, {encoding:'utf8', flag:'r'});
 
-    subscriptions += "!------ filters/" + file + " ------";
+    subscriptions += "!------ filters/" + file + " ------\n";
     subscriptions += data;
-}
+});
 
 fs.writeFileSync('template/subscriptions.txt', subscriptions);
